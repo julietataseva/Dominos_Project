65457fee39e81d51fd4dbc62b2f4cf27bc552817
Julieta <julietataseva@gmail.com>
2021-03-26 17:01:29 +0200

    added ingredient controller

--- src/main/java/dominos/controller/IngredientController.java
+++ src/main/java/dominos/controller/IngredientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class IngredientController {
+public class IngredientController extends AbstractController {
 
     @Autowired
     private IngredientService ingredientService;
