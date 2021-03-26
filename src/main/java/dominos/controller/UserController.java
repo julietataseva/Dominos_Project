62e4b761467e53b561d8aec955ca26f9780bc465
Julieta <julietataseva@gmail.com>
2021-03-26 17:02:37 +0200

    update user controller

--- src/main/java/dominos/controller/UserController.java
+++ src/main/java/dominos/controller/UserController.java
@@ -1,12 +1,17 @@
 package dominos.controller;
 
+import dominos.model.dto.LoginUserDTO;
 import dominos.model.dto.RegisterRequestUserDTO;
 import dominos.model.dto.RegisterResponseUserDTO;
+import dominos.model.dto.UserWithoutPasswordDTO;
 import dominos.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class UserController extends AbstractController {
@@ -17,5 +22,12 @@ public class UserController extends AbstractController {
     @PutMapping("/users")
     public RegisterResponseUserDTO register(@RequestBody RegisterRequestUserDTO userDTO) {
         return userService.addUser(userDTO);
+    }
+
+    @PostMapping("/users/login")
+    public UserWithoutPasswordDTO login(@RequestBody LoginUserDTO loginUserDTO, HttpSession session) {
+        UserWithoutPasswordDTO userWithoutPasswordDTO = userService.login(loginUserDTO);
+        session.setAttribute("LoggedUser", userWithoutPasswordDTO.getId());
+        return userWithoutPasswordDTO;
     }
 }
