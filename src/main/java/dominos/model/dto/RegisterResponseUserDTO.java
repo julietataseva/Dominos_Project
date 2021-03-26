31a7173a08e8eea87791795365005b39e99ed26f
Teodora Georgieva <tgeorgieva15@yahoo.com>
2021-03-26 13:47:51 +0200

    added register functionality

--- src/main/java/dominos/model/dto/RegisterResponseUserDTO.java
+++ src/main/java/dominos/model/dto/RegisterResponseUserDTO.java
@@ -11,24 +11,15 @@ import org.springframework.stereotype.Component;
 @Getter
 @Component
 public class RegisterResponseUserDTO {
-
     private int id;
-    //private String username;
     private String firstName;
     private String lastName;
-    //private int age;
-    //private String address;
     private String email;
-    //List<Car> cars;
 
     public RegisterResponseUserDTO(User user){
         id = user.getId();
         this.firstName = user.getFirstName();
         this.lastName = user.getLastName();
-        //username = user.getUsername();
-        //age = user.getAge();
-        //address = user.getAddress();
         email = user.getEmail();
-        //cars = user.getCars();
     }
 }
--- src/main/java/dominos/service/UserService.java
+++ src/main/java/dominos/service/UserService.java
@@ -18,81 +18,22 @@ public class UserService {
     private UserRepository userRepository;
 
     public RegisterResponseUserDTO addUser(RegisterRequestUserDTO userDTO){
-        //check if email exists
         if(userRepository.findByEmail(userDTO.getEmail()) != null){
             throw new BadRequestException("Email already exists");
         }
 
-        /*
-        if(userRepository.findByUsername(userDTO.getUsername() != null)){
-            throw new BadRequestException("Username already exists");
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        String initialPassword = userDTO.getPassword();
+        String confirmPassword = userDTO.getConfirmPassword();
+        if(!initialPassword.equals(confirmPassword)){
+            throw new BadRequestException("Passwords don't match");
         }
 
-         */
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
-        userDTO.setPassword(encoder.encode(userDTO.getPassword()));
+        String encodedPassword = encoder.encode(userDTO.getPassword());
+        userDTO.setPassword(encodedPassword);
         User user = new User(userDTO);
         user = userRepository.save(user);
         RegisterResponseUserDTO responseUserDTO = new RegisterResponseUserDTO(user);
         return responseUserDTO;
     }
-
-    /*
-    public List<UserWithoutPassDTO> getAllUsers() {
-        List<User> users = userRepository.findAll();
-        List<UserWithoutPassDTO> returnUsers = new ArrayList<>();
-        for(User u : users){
-            returnUsers.add(new UserWithoutPassDTO(u));
-        }
-        return returnUsers;
-    }
-
-    public UserWithoutPassDTO getUserById(int id) {
-        Optional<User> schrodingerUser = userRepository.findById(id);
-        if(schrodingerUser.isPresent()){
-            return new UserWithoutPassDTO(schrodingerUser.get());
-        }
-        else{
-            throw new NotFoundException("User not found");
-        }
-    }
-
-    public UserWithoutPassDTO login(LoginUserDto dto) {
-        User user = userRepository.findByUsername(dto.getUsername());
-        if(user == null){
-            throw new AuthenticationException("Wrong credentials");
-        }
-        else{
-            PasswordEncoder encoder = new BCryptPasswordEncoder();
-            if(encoder.matches(dto.getPassword(), user.getPassword())){
-                return new UserWithoutPassDTO(user);
-            }
-            else{
-                throw new AuthenticationException("Wrong credentials");
-            }
-        }
-    }
-
-    @Transactional
-    public UserWithoutPassDTO buyCar(int userId, int carId) {
-        Optional<User> sUser = userRepository.findById(userId);
-        Optional<Car> sCar = carRepository.findById(carId);
-        if(!sUser.isPresent()){
-            throw new NotFoundException("User not found");
-        }
-        if(!sCar.isPresent()){
-            throw new NotFoundException("Car not found");
-        }
-        Car car = sCar.get();
-        User user = sUser.get();
-        if(car.getOwner() != null){
-            throw new BadRequestException("Car already bought");
-        }
-        car.setOwner(user);
-        carRepository.save(car);//update cars set owner_id = 15 where id = 1
-        return new UserWithoutPassDTO(userRepository.findById(userId).get());
-    }
-
-     */
-
 }
