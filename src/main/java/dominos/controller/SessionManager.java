e8e1f6dd787f17bab8da605a65e337bdc9340fbd
Teodora Georgieva <tgeorgieva15@yahoo.com>
2021-03-26 14:04:02 +0200

    added SessionManager class

---
+++ src/main/java/dominos/controller/SessionManager.java
@@ -0,0 +1,36 @@
+package dominos.controller;
+
+import dominos.exceptions.AuthenticationException;
+import dominos.model.pojo.User;
+import dominos.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpSession;
+
+@Component
+public class SessionManager {
+
+    private static final String LOGGED_USER_ID = "LOGGED_USER_ID";
+
+    @Autowired
+    private UserRepository repository;
+
+    public User getLoggedUser(HttpSession session){
+        if(session.getAttribute(LOGGED_USER_ID) == null){
+            throw new AuthenticationException("You have to log in!");
+        }
+        else{
+            int userId = (int) session.getAttribute(LOGGED_USER_ID);
+            return repository.findById(userId).get();
+        }
+    }
+
+    public void loginUser(HttpSession ses, int id) {
+        ses.setAttribute(LOGGED_USER_ID, id);
+    }
+
+    public void logoutUser(HttpSession ses) {
+        ses.invalidate();
+    }
+}
