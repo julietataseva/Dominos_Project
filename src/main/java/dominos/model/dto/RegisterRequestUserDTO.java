bebbf469821d42572ea8943355e4e192cc46e48f
Teodora Georgieva <tgeorgieva15@yahoo.com>
2021-03-26 12:43:50 +0200

    minor changes

--- src/main/java/dominos/model/dto/RegisterRequestUserDTO.java
+++ src/main/java/dominos/model/dto/RegisterRequestUserDTO.java
@@ -14,9 +14,9 @@ public class RegisterRequestUserDTO {
     //private String username;
     private String firstName;
     private String lastName;
+    private String email;
     private String password;
     private String confirmPassword;
     //private int age;
     //private String address;
-    private String email;
 }
