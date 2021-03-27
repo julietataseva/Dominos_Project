25e61f171c9cb10dae9e928e241641d1de3ca17b
Teodora Georgieva <tgeorgieva15@yahoo.com>
2021-03-27 17:41:50 +0200

    added class PizzaController

--- src/main/java/dominos/controller/PizzaController.java
+++ src/main/java/dominos/controller/PizzaController.java
@@ -1,11 +1,13 @@
 package dominos.controller;
 
+import dominos.exceptions.AuthenticationException;
 import dominos.model.dao.PizzaDAO;
 import dominos.model.dto.PizzaResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -13,11 +15,17 @@ import java.util.List;
 public class PizzaController extends AbstractController{
 
     @Autowired
-
     private PizzaDAO pizzaDAO;
 
+    @Autowired
+    private SessionManager sessionManager;
+
     @GetMapping("/menu/pizzas")
-    public List<PizzaResponseDTO> getMenu() throws SQLException {
+    public List<PizzaResponseDTO> getMenu(HttpSession session) throws SQLException {
+        if(!sessionManager.validateLogged(session)){
+            throw new AuthenticationException("You have to log in to see the menu.");
+        }
+
         return pizzaDAO.getMenu();
     }
 }
