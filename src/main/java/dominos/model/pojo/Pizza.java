afe025ca5b1684b30a35fd67cbc0a2c964b54296
Julieta <julietataseva@gmail.com>
2021-03-27 18:16:00 +0200

    added connections to tables

--- src/main/java/dominos/model/pojo/Pizza.java
+++ src/main/java/dominos/model/pojo/Pizza.java
@@ -9,11 +9,17 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "pizzas")
 public class Pizza {
+    @Id
     private int id;
     private String name;
     private double price;
-
+    @ManyToOne
+    @JoinColumn(name = "size_id", nullable = false)
     private PizzaSize size;
+    @ManyToOne
+    @JoinColumn(name = "dough_type_id", nullable = false)
     private Dough dough;
 }
