deea07dfdc85c5dc4d3109a111c84fd8f0f16577
Julieta <julietataseva@gmail.com>
2021-03-28 11:11:59 +0300

    added gel all made orders by user id functionality

---
+++ src/main/java/dominos/controller/OrderController.java
@@ -0,0 +1,34 @@
+package dominos.controller;
+
+import dominos.exceptions.AuthenticationException;
+import dominos.model.dao.OrderDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class OrderController extends AbstractController {
+
+    @Autowired
+    private OrderDAO orderDAO;
+
+    @Autowired
+    private SessionManager sessionManager;
+
+    @GetMapping("/users/{id}/orders")
+    public Map<Integer, Map<LocalDate, List<String>>> getAllMadeOrdersByUserId(@PathVariable int id, HttpSession session) throws SQLException {
+        if (!sessionManager.validateLogged(session)) {
+            throw new AuthenticationException("You have to log in!");
+        }
+
+        Map<Integer, Map<LocalDate, List<String>>> orders = orderDAO.getAllMadeOrdersByUserId(id);
+        return orders;
+    }
+}
---
+++ src/main/java/dominos/model/dao/OrderDAO.java
@@ -0,0 +1,63 @@
+package dominos.model.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class OrderDAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private static final String GET_ALL_MADE_ORDERS_BY_USER_ID =
+                    "SELECT o.id, o.created_at, p.name FROM orders AS o\n" +
+                    "JOIN orders_have_pizzas AS ohp\n" +
+                    "ON o.id = ohp.order_id\n" +
+                    "JOIN pizzas AS p\n" +
+                    "ON ohp.pizza_id = p.id\n" +
+                    "UNION\n" +
+                    "SELECT o.id, o.created_at, ap.name FROM orders AS o\n" +
+                    "JOIN orders_have_additional_products AS ohap\n" +
+                    "ON o.id = ohap.order_id\n" +
+                    "JOIN additional_products AS ap\n" +
+                    "ON ohap.additional_product_id = ap.id\n" +
+                    "WHERE o.owner_id = ?\n" +
+                    "ORDER BY created_at DESC;";
+
+    public Map<Integer, Map<LocalDate, List<String>>> getAllMadeOrdersByUserId(int id) throws SQLException {
+        Map<Integer, Map<LocalDate, List<String>>> result = new HashMap<>();
+
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection();
+             PreparedStatement statement = connection.prepareStatement(GET_ALL_MADE_ORDERS_BY_USER_ID)) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int orderId = resultSet.getInt("id");
+                LocalDate orderDate = resultSet.getTimestamp("created_at").toLocalDateTime().toLocalDate();
+                String productName = resultSet.getString("name");
+
+                if (!result.containsKey(orderId)) {
+                    result.put(orderId, new HashMap<>());
+                }
+
+                if (!result.get(orderId).containsKey(orderDate)) {
+                    result.get(orderId).put(orderDate, new ArrayList<>());
+                }
+
+                result.get(orderId).get(orderDate).add(productName);
+            }
+        }
+        return result;
+    }
+}
