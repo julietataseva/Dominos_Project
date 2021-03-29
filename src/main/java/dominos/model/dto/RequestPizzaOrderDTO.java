1e754730711d48e66b3a07c3eaeb0de17ec25bfa
Teodora Georgieva <tgeorgieva15@yahoo.com>
2021-03-29 16:39:09 +0300

    add pizza to cart initialized

---
+++ src/main/java/dominos/model/dto/RequestPizzaOrderDTO.java
@@ -0,0 +1,16 @@
+package dominos.model.dto;
+
+
+import dominos.model.pojo.IProduct;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@Component
+public class RequestPizzaOrderDTO implements IProduct {
+
+}
