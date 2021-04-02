0f626535b775b93b9ea8535dc6db4175af977b31
Julieta <julietataseva@gmail.com>
2021-04-02 18:44:36 +0300

    added SuccessDTO

---
+++ src/main/java/dominos/model/dto/SuccessDTO.java
@@ -0,0 +1,16 @@
+package dominos.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Component
+public class SuccessDTO {
+    private String message;
+}
